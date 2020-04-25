f7d68b672f39c666100ef7fa8c76339ce70ad9f8
ludgart <1493675+ludgart@users.noreply.github.com>
2020-04-25 23:01:05 +0200

    Added Title

---
+++ src/minecraft/core/zocker/pro/util/Title.java
@@ -0,0 +1,95 @@
+package minecraft.core.zocker.pro.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Constructor;
+import java.util.Objects;
+
+public class Title {
+
+	private static final String serverVersion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+
+	private static void sendPacket(Player player, Object packet) {
+		try {
+			Object handle = player.getClass().getMethod("getHandle").invoke(player);
+			Object playerConnection = handle.getClass().getField("playerConnection").get(handle);
+			playerConnection.getClass().getMethod("sendPacket", getNMSClass("Packet")).invoke(playerConnection, packet);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static Class<?> getNMSClass(String name) {
+		try {
+			return Class.forName("net.minecraft.server." + serverVersion + "." + name);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void sendTitle(Player player, Integer fadeIn, Integer stay, Integer fadeOut, String title, String subtitle) {
+		if (player == null) return;
+
+		if (fadeIn == 0) {
+			fadeIn = 10;
+		}
+
+		if (stay == 0) {
+			stay = 30;
+		}
+
+		try {
+			Object e;
+			Object chatTitle;
+			Object chatSubtitle;
+			Constructor subtitleConstructor;
+			Object titlePacket;
+			Object subtitlePacket;
+
+			if (title.length() > 0) {
+				title = ChatColor.translateAlternateColorCodes('&', title);
+				title = title.replaceAll("%player%", player.getDisplayName());
+				// Times packets
+				e = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0].getField("TIMES").get(null);
+				chatTitle = Objects.requireNonNull(getNMSClass("IChatBaseComponent")).getDeclaredClasses()[0].getMethod("a", new Class[]{String.class}).invoke(null, "{\"text\":\"" + title + "\"}");
+				subtitleConstructor = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getConstructor(Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"), Integer.TYPE, Integer.TYPE, Integer.TYPE);
+				titlePacket = subtitleConstructor.newInstance(e, chatTitle, fadeIn, stay, fadeOut);
+				sendPacket(player, titlePacket);
+
+				e = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0].getField("TITLE").get(null);
+				chatTitle = Objects.requireNonNull(getNMSClass("IChatBaseComponent")).getDeclaredClasses()[0].getMethod("a", new Class[]{String.class}).invoke(null, "{\"text\":\"" + title + "\"}");
+				subtitleConstructor = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getConstructor(Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"));
+				titlePacket = subtitleConstructor.newInstance(e, chatTitle);
+				sendPacket(player, titlePacket);
+			}
+
+			if (subtitle.length() > 0) {
+				subtitle = ChatColor.translateAlternateColorCodes('&', subtitle);
+				subtitle = subtitle.replaceAll("%player%", player.getDisplayName());
+				// Times packets
+				e = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0].getField("TIMES").get(null);
+				chatSubtitle = Objects.requireNonNull(getNMSClass("IChatBaseComponent")).getDeclaredClasses()[0].getMethod("a", new Class[]{String.class}).invoke(null, "{\"text\":\"" + title + "\"}");
+				subtitleConstructor = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getConstructor(Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"), Integer.TYPE, Integer.TYPE, Integer.TYPE);
+				subtitlePacket = subtitleConstructor.newInstance(e, chatSubtitle, fadeIn, stay, fadeOut);
+				sendPacket(player, subtitlePacket);
+
+				e = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0].getField("SUBTITLE").get(null);
+				chatSubtitle = Objects.requireNonNull(getNMSClass("IChatBaseComponent")).getDeclaredClasses()[0].getMethod("a", new Class[]{String.class}).invoke(null, "{\"text\":\"" + subtitle + "\"}");
+				subtitleConstructor = Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getConstructor(Objects.requireNonNull(getNMSClass("PacketPlayOutTitle")).getDeclaredClasses()[0], getNMSClass("IChatBaseComponent"), Integer.TYPE, Integer.TYPE, Integer.TYPE);
+				subtitlePacket = subtitleConstructor.newInstance(e, chatSubtitle, fadeIn, stay, fadeOut);
+				sendPacket(player, subtitlePacket);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void clear(Player player) {
+		if (player == null) return;
+
+		sendTitle(player, 0, 0, 0, "", "");
+	}
+}
