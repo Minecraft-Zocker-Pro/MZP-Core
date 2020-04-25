f01170e5242a32969d4c576d8573e4f500d13888
ludgart <1493675+ludgart@users.noreply.github.com>
2020-04-25 22:59:46 +0200

    Added Config

---
+++ src/minecraft/core/zocker/pro/config/Config.java
@@ -0,0 +1,229 @@
+package minecraft.core.zocker.pro.config;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Config {
+
+	private static List<Config> CONFIGS = new ArrayList<>();
+
+	private String pluginName;
+	private String name;
+	private String path;
+	private YamlConfiguration configuration;
+
+	public Config(String fileName, String pluginName) {
+		new Config(fileName, pluginName, "plugins/" + pluginName + "/");
+	}
+
+	public Config(String fileName, String pluginName, String path) {
+		for (Config config : CONFIGS) {
+			if (config.getPluginName().equalsIgnoreCase(pluginName) && config.getName().equalsIgnoreCase(fileName)) return;
+		}
+
+		this.name = fileName;
+		this.pluginName = pluginName;
+		this.path = "plugins/" + pluginName + "/";
+
+		File file = new File(path + name);
+		File folder = new File(path);
+
+		try {
+			if (!folder.exists()) {
+				folder.mkdirs();
+			}
+
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			this.configuration = YamlConfiguration.loadConfiguration(file);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		CONFIGS.add(this);
+	}
+
+	public void set(String keyPath, Object value) {
+		this.configuration.set(keyPath, value);
+	}
+
+	public ConfigurationSection getSection(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getConfigurationSection(keyPath);
+		}
+		return null;
+	}
+
+	public String getSectionString(String section, String keyPath) {
+		if (keyPath.length() > 0) {
+			String value = this.getSection(section).getString(keyPath);
+			if (value != null) {
+				return value.replaceAll("&", "§");
+			}
+		}
+		return null;
+	}
+
+	public List<String> getSectionStringList(String section, String keyPath) {
+		if (keyPath.length() > 0) {
+			List<String> values = this.getSection(section).getStringList(keyPath);
+			List<String> newValues = new ArrayList<>();
+			if (!values.isEmpty()) {
+				for (String string : values) {
+					newValues.add(string.replaceAll("&", "§"));
+				}
+			}
+			return newValues;
+		}
+		return null;
+	}
+
+	/**
+	 * Read the key from the file
+	 *
+	 * @param keyPath The path to read the key
+	 * @return
+	 */
+	public String getString(String keyPath) {
+		if (keyPath == null) throw new NullPointerException("Config keypath is null.");
+
+		String value = this.configuration.getString(keyPath);
+		if (value == null) return null;
+		value = value.replaceAll("&", "§");
+		return value;
+	}
+
+	/**
+	 * Read the key from the file and replace the & symbol to § for showing colours
+	 *
+	 * @param keyPath     The path to read the key
+	 * @param formatColor Convert & to § for showing colours
+	 * @return String
+	 */
+	public String getString(String keyPath, boolean formatColor) {
+		if (keyPath == null) throw new NullPointerException("Config keypath is null.");
+
+		String value = this.configuration.getString(keyPath);
+		if (value == null) return null;
+		if (!formatColor) {
+			return value;
+		}
+
+		value = value.replaceAll("&", "§");
+		return value;
+	}
+
+	public List<String> getStringList(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getStringList(keyPath);
+		}
+		return null;
+	}
+
+	public List<Boolean> getBooleanList(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getBooleanList(keyPath);
+		}
+		return null;
+	}
+
+	public List<Integer> getIntList(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getIntegerList(keyPath);
+		}
+		return null;
+	}
+
+	public List<Double> getDoubleList(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getDoubleList(keyPath);
+		}
+		return null;
+	}
+
+	public List<Float> getFloatList(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getFloatList(keyPath);
+		}
+		return null;
+	}
+
+	public List<Long> getLongList(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getLongList(keyPath);
+		}
+		return null;
+	}
+
+	public List<?> getList(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getList(keyPath);
+		}
+		return null;
+	}
+
+	public int getInt(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getInt(keyPath);
+		}
+		return 0;
+	}
+
+	public double getDouble(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getDouble(keyPath);
+		}
+		return 0;
+	}
+
+	public boolean getBool(String keyPath) {
+		if (keyPath.length() > 0) {
+			return this.configuration.getBoolean(keyPath);
+		}
+
+		return false;
+	}
+
+	public long getLong(String keyPath) {
+		return this.configuration.getLong(keyPath);
+	}
+
+	public void save() {
+		try {
+			this.configuration.save(new File(this.path + this.name));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void reload() {
+		this.configuration = YamlConfiguration.loadConfiguration(new File(path + name));
+	}
+
+	public String getPluginName() {
+		return this.pluginName;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getPath() {
+		return this.path;
+	}
+
+	public static Config getConfig(String fileName, String pluginName) {
+		for (Config config : CONFIGS) {
+			if (config.getPluginName().equalsIgnoreCase(pluginName) && config.getName().equalsIgnoreCase(fileName)) return config;
+		}
+		return null;
+	}
+}
